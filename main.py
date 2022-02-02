43dd4cff612815698c275bc02ec89063e9dd35fe
moqi <moqimoqidea@gmail.com>
2022-02-02 13:30:55 +0800

    Reformat to Function

--- main.py
+++ main.py
@@ -1,37 +1,36 @@
 import csv
 from pathlib import Path
 
-if __name__ == '__main__':
-    header = ['Highlight', 'Chapter', 'Title', 'Author', 'Note']
-    data = []
 
-    source = 'test_data/source.md'
-    target_v1 = 'test_data/target_v1.csv'
-
+def get_title_and_author(source):
     line_array = open(source, 'r', encoding='utf-8').readlines()
     title = line_array[0][2:-1]
-    print(title)
     author = line_array[2][3:-3]
-    print(author)
+    return title, author
+
+
+def markdown_to_csv(source, target):
+    csv_header = ['Highlight', 'Chapter', 'Title', 'Author', 'Note']
+    csv_data = []
+
+    title_author_tuple = get_title_and_author(source)
+    title, author = title_author_tuple[0], title_author_tuple[1]
 
     txt = Path(source).read_text()
-
     chapters = txt.split('###')
-    print(len(chapters))
 
     for chapter in chapters:
-        # skip non standard chapters
         if chapter[0] != ' ':
             continue
 
         rows = chapter.split('\n\n')
         chapter_name = rows[0][1:]
-        print(chapter_name)
+
+        if chapter_name == '最终点评':
+            csv_data.append(['', chapter_name, title, author, rows[1][2:]])
+            continue
 
         for row in rows[1:-1]:
-            highlight = ''
-            note = ''
-
             if row.startswith('* '):
                 highlight = row[2:]
             elif row.startswith('\n* '):
@@ -39,20 +38,20 @@ if __name__ == '__main__':
             else:
                 continue
 
-            print(highlight)
-
             if highlight.count('（个人笔记:') != 0:
                 note = highlight[highlight.index('（个人笔记:') + 7:][:-1]
-                print(note)
+            else:
+                note = ''
 
-            data.append([highlight, chapter_name, title, author, note])
+            csv_data.append([highlight, chapter_name, title, author, note])
 
-    print(len(data))
-    print(data)
-
-    with open(target_v1, 'w', encoding='UTF8') as f:
+    with open(target, 'w', encoding='UTF8') as f:
         writer = csv.writer(f)
-        writer.writerow(header)
+        writer.writerow(csv_header)
 
-        for row in data:
+        for row in csv_data:
             writer.writerow(row)
+
+
+if __name__ == '__main__':
+    markdown_to_csv('test_data/source.md', 'test_data/target.csv')
