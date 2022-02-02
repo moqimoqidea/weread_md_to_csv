f685be97ceab77066679a5fbebafe328b92f373b
moqi <moqimoqidea@gmail.com>
2022-02-02 15:54:35 +0800

    Handle main with User use

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import csv
+import os
+import sys
 from pathlib import Path
 
 
@@ -10,12 +12,10 @@ def get_title_and_author(source):
 
 
 def markdown_to_csv(source, target):
-    csv_header = ['Highlight', 'Chapter', 'Title', 'Author', 'Note']
-    csv_data = []
-
     title_author_tuple = get_title_and_author(source)
     title, author = title_author_tuple[0], title_author_tuple[1]
 
+    csv_data = []
     txt = Path(source).read_text()
     chapters = txt.split('###')
 
@@ -47,11 +47,43 @@ def markdown_to_csv(source, target):
 
     with open(target, 'w', encoding='UTF8') as f:
         writer = csv.writer(f)
-        writer.writerow(csv_header)
+        writer.writerow(['Highlight', 'Chapter', 'Title', 'Author', 'Note'])
 
         for row in csv_data:
             writer.writerow(row)
 
 
 if __name__ == '__main__':
-    markdown_to_csv('test_data/source.md', 'test_data/target.csv')
+    first_argv = sys.argv[1]
+
+    if first_argv == '-h':
+        print('Usage: python main.py source.md target.csv')
+        sys.exit(0)
+
+    if not os.path.exists(first_argv):
+        print('Path: {} does not exist.'.format(first_argv))
+        sys.exit(0)
+
+    if os.path.isdir(first_argv):
+        print('Path: {} is a directory.\n'.format(first_argv))
+        list_file = os.listdir(first_argv)
+        for file in list_file:
+            if file.endswith('.md'):
+                print('Converting {} to csv...'.format(file))
+                markdown_to_csv(first_argv + '/' + file, first_argv + '/' + file[:-2] + 'csv')
+        print('\nAll md converting Done.')
+        sys.exit(0)
+
+    if not first_argv.endswith('.md'):
+        print('File: {} is not a markdown file.'.format(first_argv))
+        sys.exit(0)
+
+    source_md = first_argv
+    target_csv = first_argv[:-2] + 'csv'
+    if len(sys.argv) >= 3:
+        target_csv = os.path.dirname(source_md) + '/' + sys.argv[2]
+    else:
+        target_csv = source_md[:-2] + 'csv'
+
+    markdown_to_csv(source_md, target_csv)
+    print('Converting {} to csv {}, done'.format(source_md, target_csv))
